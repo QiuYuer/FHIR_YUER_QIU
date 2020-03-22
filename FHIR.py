0d518ea327118f2aa63c645dddbb4ae89a760cca
QiuYuer <zcabyqi@ucl.ac.uk>
2020-03-22 20:39:09 +0000

    first commit

--- FHIR.py
+++ FHIR.py
@@ -8,9 +8,7 @@ class FHIR_response():
         self.__CLIENT_SECRET = "ux@CJAaxCD85A9psm-Wdb?x3/Z4c6gp9"
         self.__SCOPE = "https://gosh-fhir-synth.azurehealthcareapis.com/.default"
         self.__FHIR_BASE_URL = "https://gosh-fhir-synth.azurehealthcareapis.com"
-        self.__payload = "grant_type=client_credentials&client_id={}&client_secret={}&scope={}".format(self.__CLIENT_ID,
-                                                                                                       self.__CLIENT_SECRET,
-                                                                                                       self.__SCOPE)
+        self.__payload = "grant_type=client_credentials&client_id={}&client_secret={}&scope={}".format(self.__CLIENT_ID, self.__CLIENT_SECRET, self.__SCOPE)
         self.__url = "https://login.microsoftonline.com/ca254449-06ec-4e1d-a3c9-f8b84e2afe3f/oauth2/v2.0/token"
         self.__headers = {'content-type': "application/x-www-form-urlencoded"}
         self.__access_token = self.get_access_token()
@@ -28,19 +26,17 @@ class FHIR_response():
     def get_all_patients(self):
         res = requests.get('{}/Patient'.format(self.__FHIR_BASE_URL), headers=self.__auth_header)
         json = res.json()
-        result = self.__get_all_patients_page(json, [])
+        result = self.get_all_patients_page(json, [])
 
         patients = []
         for patient_json_data in result:
             for patient_data in patient_json_data["entry"]:
                 patients.append(Patient.Create_Patient().Test_Jsonfile(patient_data))
-        print(len(patients))
         return patients
 
-    def __get_all_patients_page(self, json, list):
+    def get_all_patients_page(self, json, list):
         if (json == None):
             return
-
         relation = json["link"][0]["relation"]
         list.append(json)
 
@@ -48,6 +44,6 @@ class FHIR_response():
             url = json["link"][0]["url"]
             res = requests.get(url, headers=self.__auth_header)
             json = res.json()
-            return self.__get_all_patients_page(json, list)
+            return self.get_all_patients_page(json, list)
         else:
             return list
